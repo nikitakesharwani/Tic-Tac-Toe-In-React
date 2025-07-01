74342abbaadbcfe00eb18cd80704c721bdcd8193
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-07-01 08:06:00 +0530

    winner logic added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
 import "./App.css";
-import { useState } from "react";
+import Board from "./Component/Board";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
+function App() {
+  return <Board />;
 }
 
-export default function App() {
-  return (
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </>
-  );
-}
+export default App;
